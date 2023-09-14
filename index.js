b9c27a61790d519eb96e18b80b4d7107d6cc6740
Mason Michalec <48143397+michalecm@users.noreply.github.com>
2023-09-14 09:47:03 +0200

    complete task 2

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const repl = require('repl')
+const { EventEmitter } = require('events')
+const childProcess = require('child_process')
+const fs = require('fs')
 
-module.exports = function getRandomNumber() {
-    return Math.random() * Number.MAX_SAFE_INTEGER
+function emitStats() {
+    let count = 0;
+    let msInterval = 100;
+    const task = 'ps -A -o %cpu,%mem,comm | sort -nr | head -n 1';
+    const emitter = new EventEmitter();
+    emitter.on('result', (result) => {
+        console.clear();
+        console.log(result)
+    })
+
+    const interval = setInterval(() => {
+    
+        childProcess.exec(task, (err, stdout, stderr) => {
+            emitter.emit('result', stdout);
+
+            if(count % 5000 === 0){
+                fs.open('activityMonitor.log', 'a', (err, fileDescriptor) => {
+                    fs.appendFile(fileDescriptor, stdout, (err) => {
+                        console.error("There was an error appending to file!")
+                    })
+                    fs.close(fileDescriptor)
+                })
+            }
+        })
+
+        if(count === 15000) clearInterval(interval);
+        count += msInterval;
+    }, msInterval)
 }
 
-repl.start();
-localStorage.on('exit', () => {
-    console.log("Bye!")
-    process.exit();
-})
+emitStats();
